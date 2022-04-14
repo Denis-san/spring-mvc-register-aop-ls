736ef476aa4144296dd421a28f3ec0f718493d55
Denis-san <denilson.nt.nc@gmail.com>
2022-04-14 11:17:55 -0300

    project created

---
+++ src/main/java/br/com/san/ls/SpringMvcRegisterAopLsApplication.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRegisterAopLsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRegisterAopLsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/san/ls/SpringMvcRegisterAopLsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRegisterAopLsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
